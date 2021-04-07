65c5f7564c0d310c4ecdc9e9bfadbb6043e7fb76
chris carr <ccarr895@gmail.com>
2021-04-07 13:50:55 +0100

    included nump corrcoef

--- particle_swarm/tests/linear_regression.py
+++ particle_swarm/tests/linear_regression.py
@@ -1,8 +1,17 @@
+from math import sqrt
 from pathlib import Path
 
 import numpy as np
 
-FILENAME = 'boston.csv'
+FILENAME = "boston.csv"
 
-def boston(x):
+
+def boston(z):
     data = np.loadtxt(Path.cwd() / FILENAME)
+
+
+    coef = np.corrcoef(data)
+    print(coef)
+    np.set_printoptions(suppress=True)  # non-scientific notation
+
+boston(1)
