44b0b3a021473a361f3ea31436b3d9459db943af
chris carr <ccarr895@gmail.com>
2021-04-08 15:56:55 +0100

    included matplotlib

--- particle_swarm/tests/linear_regression.py
+++ particle_swarm/tests/linear_regression.py
@@ -1,6 +1,7 @@
 from math import sqrt
 from pathlib import Path
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 FILENAME = "boston.csv"
@@ -9,9 +10,19 @@ FILENAME = "boston.csv"
 def boston(z):
     data = np.loadtxt(Path.cwd() / FILENAME)
 
+    y = data[..., 13]
+    x = data[:, 5]
 
-    coef = np.corrcoef(data)
-    print(coef)
+    model = np.poly1d(np.polyfit(x, y, 3))
+    line = np.linspace(np.min(x), np.max(x))
+
+    print("x", x)
+    print("y", y)
+
+    plt.scatter(x, y)
+    plt.plot(line, model(line))
+    plt.show()
     np.set_printoptions(suppress=True)  # non-scientific notation
+
 
 boston(1)
